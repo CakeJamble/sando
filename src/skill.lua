5c4e7163107dbc1551e199a35afae77649839f1d
Jacob Campbell <104522681+campjake@users.noreply.github.com>
2024-09-09 15:53:55 -0700

    Tied together projectiles with the skill class

--- src/skill.lua
+++ src/skill.lua
@@ -1,4 +1,6 @@
 --! filename: skill
+require('projectile')
+
 local class = require 'libs/middleclass'
 Skill = class('Skill')
 
@@ -7,8 +9,13 @@ Skill = class('Skill')
     -- postconditions: A Skill with an animation appended to the skill dict
 function Skill:initialize(t, width, height)
   self.skill = t
+  self.hitType = t['hit_type']
   self.animation = Skill:newAnimation(self.skill['sprite_path'], width, height, self.skill['duration'])
-  
+  projectiles = {}
+  self.frameCount = 0
+  self.projectileCount = 0
+  self.projectileRate = t['projectile_rate']
+  self.projectileCountLimit = t['projectile_count']
   self.projectileAnimation = Skill:newAnimation(self.skill['projectile_path'], self.skill['projectile_width'], self.skill['projectile_height'], self.skill['duration'])
 end;
 
@@ -34,6 +41,30 @@ function Skill:newAnimation(path, width, height, duration)
   return animation
 end;
 
+function Skill:update(dt)
+  if self.hitType == 'projectile' then
+    self.frameCount = self.frameCount + 1
+    
+    if self.frameCount >= self.projectileRate and self.projectileCount < self.projectileCountLimit then
+      table.insert(projectiles, Projectile(self.projectileAnimation, userX, userY, userWidth, userHeight, targetX, targetY, dx, dy, a, r, tR, damage))
+      self.projectileCount = self.projectileCount + 1
+    end
+    
+    for i, projectile in pairs(projectiles) do
+      projectile:update(dt)
+    end
+    
+  end
+end;
+    
+
 function Skill:draw()
   love.graphics.draw(animation.spriteSheet, animation.frames[math.floor(animation.currentFrame)], 100, 100)
+  
+  if self.hitType == 'projectile' then 
+    for i, projectile in pairs(projectiles) do
+      projectile:draw()
+    end
+  end
+  
 end;
