88149a328d210de4a647b522e7eef474030fa9e3
campjake <104522681+campjake@users.noreply.github.com>
2024-10-09 11:37:34 -0700

    New file with high level doc on structure and todo

---
+++ src/encounter_generator.lua
@@ -0,0 +1,13 @@
+--! file: encounter_generator
+
+
+-- TODO: needs enemy pools (new file needed)
+
+-- TODO: needs encounter types (in file)
+
+-- TODO: needs challenge ratings for encounters & XP thresholds to define encounter rewards
+  -- check DnD balance system for design philosophy
+  
+  -- should be managed in spreadsheets
+
+-- 
