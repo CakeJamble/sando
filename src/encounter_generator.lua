53dd30954e0d5b1d643cf1e0792e96d1aeb67464
campjake <104522681+campjake@users.noreply.github.com>
2024-11-04 10:44:41 -0800

    added enemy pools and refactored encounter gen

--- src/encounter_generator.lua
+++ src/encounter_generator.lua
@@ -7,37 +7,57 @@ require('gamestates/game')
 
 -- Need to port over map generation techniques for weighted random creation of encounters!!!
 function generateEncounter(floorNum)
-  team = Team()
+  enemyTeam = Team()
   encounteredPools = {}
   if floorNum < 10 then
     -- Weighted Randomly grab from Enemy Pool 1
     -- TODO : Make this actually weighted rand
-    local encounter = math.random(1, 10)
-    populateTeam(encounter, team)
+    local encounter = math.random(1, #enemyPool1)
+    populateTeam(encounter, enemyTeam)
     table.insert(encounteredPools, encounter)
   elseif floorNum == 10 then
     -- Randomly grab from Boss Pool 1
-    local encounter = math.random(1, 4)
-    populateTeam(encounter, team)
+    local encounter = math.random(1, #bossPool1)
+    populateTeam(encounter, enemyTeam)
     table.insert(encounteredPools, encounter)
   elseif floorNum < 20 then
     -- Weighted Randomly grab from Enemy Pool 2
     -- TODO : Make this actually weighted rand
-    local encounter = math.random(11, 20)
-    populateTeam(encounter, team)
+    local encounter = math.random(1, #enemyPool2)
+    populateTeam(encounter, enemyTeam)
     table.insert(encounteredPools, encounter)
   elseif floorNum == 20 then
-    -- Randomly grab from Boss Pool 2
+    local encounter = math.random(1, #bossPool2)
+    populateTeam(encounter, enemyTeam)
+    
 -- PLEASE BEGIN HERE comment from 10/31/2024
   end
-  return team
+  return enemyTeam
 end;
 
 -- Grabs team from pools listed below
-function populateTeam(encounter, team)
+-- NOTE: completely random, needs refacor. Just for testing loading of enemies for now
+function populateTeam(encounter, newTeam)
+  for i=1,encounter do
+    -- need a way to dynamically generate an enemy from just a name??
+    -- local enemy = Enemy
 end;
 
 -- Create tables for encounter pools
 enemyPool1 = {
+  {
+    'Sortilla',
+    'Reggie',
+  },
   
+  {
+    'Line'
+  },
+  
+  {
+    'Goki',
+    'Dasbunny'
+  }
+}
+
   
