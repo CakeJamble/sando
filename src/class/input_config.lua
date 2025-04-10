cb52edd6dd326281d9531b4ce0b7be6f6b776caf
campjake <104522681+CakeJamble@users.noreply.github.com>
2025-04-10 12:08:53 -0700

    Create input_config.lua

---
+++ src/class/input_config.lua
@@ -0,0 +1,63 @@
+--! filename: input config
+
+Class = require 'libs.hump.class'
+InputConfig = Class{}
+
+function InputConfig:init()
+  self.listeners = {}
+  self.xboxInputs = {   -- gamepad button : timestamp
+    ['a'] = {},
+    ['b'] = {},
+    ['x'] = {},
+    ['y'] = {},
+    ['back'] = {},
+    ['start'] = {},
+    ['leftshoulder'] = {},
+    ['rightshoulder'] = {},
+    ['dpup'] = {},
+    ['dpdown'] = {},
+    ['dpleft'] = {},
+    ['dpright'] = {}
+  }
+  self.currentControllerInputs = xboxInputs
+  self.timeStamp = 0
+  self.listening = false
+end;
+
+function InputConfig:update()
+  if self.listening then
+    self.timeStamp += 1
+  end
+end;
+
+function InputConfig:resetTimeStamp()
+  self.timeStamp = 0
+end;
+
+function InputConfig:insertInput(input, timeStamp)
+  table.insert(self.currentControllerInputs[input], timeStamp)
+end;
+
+function InputConfig:validateQTE(input, qteWindow) --> bool
+  local timeStamps = self.currentControllerInputs[input]
+  for i=1, #timeStamps do
+    if isWithinWindow(timeStamps[i], qteWindow) then
+      return true
+    end
+  end
+  return false
+end;
+
+function InputConfig:isWithinWindow(timeStamp, qteWindow)
+  local windowOpen = timeStamp[1]
+  local windowClose = timeStamp[2]
+  
+  return timeStamp >= windowOpen and timeStamp <= windowClose
+end;
+
+function emitSignal()
+  Signal.emit('input')
+end;
+
+  
+  
