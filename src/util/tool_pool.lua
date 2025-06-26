c124219a67d4559748d881dbb532a35a58d93945
cakejamble <campbell.jakelog@gmail.com>
2025-06-26 16:51:55 -0700

    wip for proc condition of tools

--- src/util/tool_pool.lua
+++ src/util/tool_pool.lua
@@ -8,155 +8,181 @@ local toolPool =
             description = 'On pickup, increase all members\' HP by 8%',
             flavorText = 'Microwaved to perfection',
             rarity = 'common'
+            proc = 'OnPickup'
         },
         {
             toolName = 'Strainer',
             description = 'On pickup, increase all member\'s defense by 1',
             flavorText = 'No clumps',
-            rarity = 'common'
+            rarity = 'common',
+            proce = 'OnPickup'
         },
         {
             toolName = 'Energy Drink',
             description = 'On pickup, assigned character has a 50% chance to take their turn first',
             flavorText = 'For the baker with nothing left to give',
             rarity = 'common',
+            proc = 'OnPickup'
         },
         {
             toolName = 'Smart Watch',
             description = 'PLACEHOLDER',
             flavorText = 'Steps add up',
             rarity = 'common',
+            proc = 'PLACEHOLDER'
         },
         {
             toolName = 'Boiled Egg',
             description = 'On pickup, enchant a move of one character with healing',
             flavorText = 'It\'s close to hatching',
             rarity = 'common',
+            proc = 'OnPickup'
         },
         {
             toolName = 'Splatter Guard',
             description = 'Further reduces damage blocked by guarding by a small amount',
             flavorText = 'For the baker with nothing left to give',
             rarity = 'common',
+            proc = 'OnDamaged'
         },
         {
             toolName = 'Water Bottle',
             description = 'On pickup, heal all members and status conditions',
             flavorText = 'Why is it always empty',
             rarity = 'common',
+            proc = 'OnPickup'
         },
         {
             toolName = 'Piping Bag',
-            description = 'Flour Attacks cost 1 less Flour Point',
+            description = 'On pickup, all current Flour Skill costs are reduced by 1 FP',
             flavorText = 'CUSTARD',
             rarity = 'common',
+            proc = 'OnPickup'
         },
         {
             toolName = 'Banneton',
-            description = 'Flower attacks cost 1 more FP but are more effective',
+            description = 'On pickup, increase FP for all current Flower Skills by 1, and increase their effectiveness',
             flavorText = 'Every boule deserves a viking funeral',
             rarity = 'common',
+            proc = 'OnPickup'
         {
             toolName = 'Hard Water',
             description = 'On level up, roll a bonus stat reward',
             flavorText = 'Good ole tap water',
             rarity = 'common',
+            proc = 'OnLevelUp'
         },
         {
             toolName = 'Ancient Interface',
             description = 'Your first attack each battle always gets the QTE bonus',
             flavorText = 'There\'s a satisfying clack to the keys',
             rarity = 'common',
+            proc = 'OnAttack'
         },
         {
             toolName = 'Misshapen Beignet',
             description = 'Enemies cannot escape',
             flavorText = '???', -- revise
             rarity = 'common',
+            prove = 'PLACEHOLDER'
         },
         {
             toolName = 'Dirty Dishes',
             description = 'Slow enemies at the start of battle',
             flavorText = 'The kitchen can\'t move when the sink is full',
             rarity = 'common',
+            proc = 'OnStartBattle'
         },
         {
             toolName = 'Fake Mustache',
             description = 'Basic attacks deal more damage',
             flavorText = 'revise',
             rarity = 'common',
+            proc = 'OnPickup'
         },
         {
             toolName = 'Decroded Mitts',
             description = 'You cannot be debuffed during your turn',
             flavorText = 'Heat has worn away the fingertips',
             rarity = 'common',
+            proc = 'OnAttack'
         },
         {
             toolName = 'Dairy Pills',
-            description = 'Cures lactose intolerance',
+            description = 'Cures lactose intolerance at the start of every turn',
             flavorText = 'I\'m so bloated',
             rarity = 'common',
+            proc = 'OnStartTurn'
         },
         {
             toolName = 'Dough Hook',
             description = 'Increase damage for each empty or cursed skill slot',
             flavorText = 'It starts with a good mix',
-            rarity = 'common'
+            rarity = 'common',
+            proc = 'OnAttack'
         },
         {
             toolName = 'Basque Cheesecake',
             description = 'Gain additional EXP when defeating burnt enemies',
             flavorText = 'It\'s not burnt! Okay, it is burnt, but it\'s supposed to look like that.',
-            rarity = 'common'
+            rarity = 'common',
+            proc = 'OnKO'
         },
         {
             toolName = 'Refurbished Idol',
             description = 'Gain additional money from enemies, equal to your level',
             flavorText = 'Their beauty was a spark that started an ancient war',
-            rarity = 'common'
+            rarity = 'common',
+            proc = 'OnKO'
         },
         {
             toolName = 'Water Cup',
             description = 'Restore a small amount of FP when you sell equipment',
             flavorText = 'Someone filled it with soda',
-            rarity = 'common'
+            rarity = 'common',
+            proc = 'OnEquipSell'
         },
         {
             toolName = 'Bottomless Fries',
             description = 'Restore a small amount of HP when purchasing an item',
             flavorText = 'It takes way too long for this to get refilled',
-            rarity = 'common'
+            rarity = 'common',
+            proc = 'OnPurchase'
         },
         {
             toolName = 'Tip Jar',
             description = 'Get slightly more money when selling accessories',
             flavorText = 'Money goes in, tips come out. You can\'t explain that',
-            rarity = 'common'
+            rarity = 'common',
+            proc = 'OnAccSell'
         },
         {
             toolName = 'Chew Toy',
             description = 'Weaken all enemies at the start of battle',
             flavorText = 'Whoever this belonged to loved this thing',
-            rarity = 'common'
+            rarity = 'common',
+            proc = 'OnStartBattle'
         },
         {
             toolName = 'Dress Shirt',
             description = 'Every 3 turns, the first character goes twice',
             flavorText = 'Lets you make a first impression twice!',
-            rarity = 'common'
+            rarity = 'common',
+            proc = 'OnStartTurn'
         },
         {
             toolName = 'Calendar',
             description = 'Start of turn, lose 1 HP every turn and deal 4 damage to all enemies',
             flavorText = 'Closed on Mondays and Tuesdays',
-            rarity = 'common'
+            rarity = 'common',
+            proc = 'OnStartTurn'
         },
         {
             toolName = 'Grease of Ruin',
             description = 'Grounded enemies have a small chance to have their attack lowered after attacking',
             flavorText = 'Non-slip shoes required',
-            rarity = 'common'
+            rarity = 'common',
+            proc = 'OnDamaged'
         },
         {
             toolName = 'Dragonscale Ring',
