990a54c84cb7c89f551fc09b5cbcfebfdd2d21ed
cakejamble <campbell.jakelog@gmail.com>
2025-08-12 13:21:40 -0700

    SG-18 Fixed pathing for item loader and skill loader

--- src/util/item_loader.lua
+++ src/util/item_loader.lua
@@ -2,19 +2,24 @@ local json = require('libs.json')
 
 local function loadItem(itemName)
 	local jsonPath = 'data/item/' .. itemName .. '.json'
-	local logicPath = 'logic/skill/' .. itemName
-
 	local raw = love.filesystem.read(jsonPath)
 	local data = json.decode(raw)
 
-	local proc = require('logic.item.' .. itemName)
+	local itemType
+	if data.itemType then
+		itemType = data.itemType .. '.'
+	else
+		error('The item: ' .. itemName .. ', did not have an itemType field defined')
+	end
+
+	local logicPath = 'logic.item.' .. itemType .. itemName
+	local proc = require(logicPath)
 	if proc then
 		data.proc = proc
 	else
 		error('Failed to find implementation for item named \'' .. itemName .. '\': ' .. tostring(proc))
 	end
 
-	item.tag = itemName
 	return data
 end
 
--- src/util/skill_loader.lua
+++ src/util/skill_loader.lua
@@ -2,12 +2,12 @@ local json = require('libs.json')
 
 local function loadSkill(skillName)
 	local jsonPath = 'data/skill/' .. skillName .. '.json'
-	local logicPath = 'logic/skill/' .. skillName
+	local logicPath = 'logic.skill.' .. skillName
 
 	local raw = love.filesystem.read(jsonPath)
 	local data = json.decode(raw)
 
-	local proc = require('logic.skill.' .. skillName)
+	local proc = require(logicPath)
 	if proc then
 		data.proc = proc
 	else
