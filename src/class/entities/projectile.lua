42130dae5d83399aa3c933c20160376443b10e17
cakejamble <campbell.jakelog@gmail.com>
2025-08-13 14:53:58 -0700

    Fixed projectile hitboxes and two targeting for flame skill

--- src/class/entities/projectile.lua
+++ src/class/entities/projectile.lua
@@ -1,17 +1,19 @@
-Projectile = Class{}
+Projectile = Class{drawHitboxes = false}
 
 function Projectile:init(x, y, index)
 	self.pos = {x=x, y=y}
 	self.dims = {r = 10}
+	local r = self.dims.r
 	self.hitbox = {
-		x=x,y=y,w=2*self.dims.r, h=2*self.dims.r
+		x=x-r,y=y-r,w=2*self.dims.r, h=2*self.dims.r
 	}
 	self.index = index
 end;
 
 function Projectile:update(dt)
-	self.hitbox.x = self.pos.x
-	self.hitbox.y = self.pos.y
+	local r = self.dims.r
+	self.hitbox.x = self.pos.x - r
+	self.hitbox.y = self.pos.y - r
 end
 
 function Projectile:draw()
@@ -23,4 +25,10 @@ function Projectile:draw()
 	 love.graphics.setColor(0, 0, 0, 0.4)
   love.graphics.ellipse("fill", self.pos.x, self.pos.y + self.hitbox.h, self.hitbox.w / 2, self.hitbox.h / 4)
   love.graphics.setColor(1, 1, 1, 1)
+
+  if Projectile.drawHitboxes then
+    love.graphics.setColor(1, 1, 0, 0.4)
+    love.graphics.rectangle("fill", self.hitbox.x, self.hitbox.y, self.hitbox.w, self.hitbox.h)
+    love.graphics.setColor(1, 1, 1)
+  end
 end;
