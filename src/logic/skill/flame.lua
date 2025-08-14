f8a1cf884ac7b99781cf6e94eeeb6d75b1b48e01
cakejamble <campbell.jakelog@gmail.com>
2025-08-14 10:44:28 -0700

    SG-24 Flame path using snake path for N-targets and rebounds on dodges

--- src/logic/skill/flame.lua
+++ src/logic/skill/flame.lua
@@ -20,11 +20,31 @@ return function(ref, qteManager)
   end
 
   -- Pathing projectile between 2 targets
-  local goalPos = {
-    {x = tPos[1].x - (2 * tPos[1].w), y = tPos[1].y + tPos[1].h},
-    {x = ref.hitbox.x - (ref.hitbox.w / 2), y = tPos[2].y + tPos[2].h},
-  }
+  -- local goalPos = {
+  --   {x = tPos[1].x - (2 * tPos[1].w), y = tPos[1].y + tPos[1].h},
+  --   {x = ref.hitbox.x - (ref.hitbox.w / 2), y = tPos[2].y + tPos[2].h},
+  -- }
 
+  local goalPos = {}
+  for i,pos in ipairs(tPos) do
+    local x, y
+    local xOffset = 2 * pos.w
+    if i % 2 == 1 then
+      x = pos.x - xOffset
+    else
+      x = pos.x + xOffset
+    end
+    y = pos.y + pos.h
+    table.insert(goalPos, {x=x,y=y})
+
+    -- Tween behind next target
+    if i < #tPos then
+      local nextTarget = tPos[i+1]
+      table.insert(goalPos, {x=x,y=nextTarget.y + nextTarget.h})
+    end
+  end
+
+  -- rebound onto user at the end (if dodged perfectly)
   table.insert(goalPos, {x = ref.hitbox.x + (ref.hitbox.w / 2), y = ref.hitbox.y + (ref.hitbox.h / 2)})
 
   local flame = Projectile(ref.pos.x - ref.hitbox.w, ref.pos.y + (ref.hitbox.h / 2), skill.castsShadow, 1)
@@ -36,31 +56,36 @@ return function(ref, qteManager)
         target:takeDamage(damage)
         hasCollided[i] = true
         flawlessDodge = false
-        print('collision at index ' .. i, target.entityName .. 'takes damage: ' .. damage)
       end
     end
   end
 
--- Tweening for 2 Targets + Rebound onto ref
-  local goalX, goalY = goalPos[1].x, goalPos[1].y
+  -- Tweening for N Targets + Rebound onto ref
   local attack = flux.to(flame.pos, flameTravelTime, {x = goalPos[1].x, y = goalPos[1].y})
     :ease(skill.beginTweenType)
     :onupdate(checkCollision)
-    :after(flame.pos, flameTravelTime, {x = goalPos[1].x, y = goalPos[2].y})
-      :ease(skill.beginTweenType)
-      :onupdate(checkCollision)
-    :after(flame.pos, flameTravelTime, {x = goalPos[2].x, y = goalPos[2].y})
-      :ease(skill.beginTweenType)
-      :onupdate(checkCollision)
-    :after(flame.pos, flameTravelTime, {x = goalPos[3].x, y = goalPos[3].y})
-      :ease(skill.beginTweenType)
-      :oncomplete(function()
+
+  for i=2, #goalPos do
+    local t = flameTravelTime
+    
+    -- Make flame faster so it can be jumped over
+    if i % 2 == 1 then
+      t = t / 3
+    end
+
+    attack = attack:after(t, {x=goalPos[i].x, y=goalPos[i].y}):ease(skill.beginTweenType):onupdate(checkCollision)
+
+    -- Rebound onto user
+    if i == #goalPos then
+      attack = attack:oncomplete(function()
         if flawlessDodge then
           ref:takeDamage(damage)
         end
         table.remove(ref.projectiles, 1)
         ref:endTurn(skill.duration)
       end)
+    end
+  end
 
   ref.tweens['attack'] = attack
 
