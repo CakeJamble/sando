3f45b9b31e58890a72abd5c7f449c99a69363637
cakejamble <campbell.jakelog@gmail.com>
2025-08-18 10:24:07 -0700

    SG-152 removed tool class and added tool loader function

---
+++ src/util/tool_loader.lua
@@ -0,0 +1,19 @@
+local json = require('libs.json')
+
+local function loadTool(toolName)
+	local jsonPath = 'data/item/tool/' .. toolName .. '.json'
+	local raw = love.filesystem.read(jsonPath)
+	local data = json.decode(raw)
+
+	local logicPath = 'logic.item.tool.' .. toolName
+	local proc = require(logicPath)
+	if proc then
+		data.proc = proc
+	else
+		error('Failed to find implementation for tool named \'' .. toolName .. '\': ' .. tostring(proc))
+	end
+
+	return data
+end;
+
+return loadTool
