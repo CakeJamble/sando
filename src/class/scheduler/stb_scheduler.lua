0b8344633a8d0bee7e22c9f62bebb4e5bb4eb3ec
cakejamble <campbell.jakelog@gmail.com>
2025-08-27 15:27:23 -0700

    SG-198 STB Scheduler class annotations

--- src/class/scheduler/stb_scheduler.lua
+++ src/class/scheduler/stb_scheduler.lua
@@ -9,7 +9,8 @@ local Timer = require('libs.hump.timer')
 local STBScheduler = Class{__includes = Scheduler}
 
 -- Standard Turn Based Scheduler
-
+---@param characterTeam CharacterTeam
+---@param enemyTeam EnemyTeam
 function STBScheduler:init(characterTeam, enemyTeam)
 	Scheduler.init(self, characterTeam, enemyTeam)
 	self:sortQueue(self.combatants)
@@ -68,6 +69,8 @@ function STBScheduler:exit()
 	self:removeSignals()
 end;
 
+---@param t Entity[]
+---@return Entity[]
 function STBScheduler:sortQueue(t)
   table.sort(t,
     function(entity1, entity2)
@@ -86,6 +89,7 @@ function STBScheduler:sortWaitingCombatants()
   end
 end;
 
+---@param command Command
 function STBScheduler:enqueueCommand(command)
   print('enqueuing command from ' .. command.entity.entityName .. ' in command queue')
   table.insert(self.commandQueue, command)
