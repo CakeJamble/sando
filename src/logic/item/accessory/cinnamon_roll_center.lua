dc66d1eb1bac2668583cbc9d79691a00c15abf3b
cakejamble <campbell.jakelog@gmail.com>
2025-09-11 12:26:26 -0700

    SG-249 refactor for designer loafers and cinnamon roll center

--- src/logic/item/accessory/cinnamon_roll_center.lua
+++ src/logic/item/accessory/cinnamon_roll_center.lua
@@ -1,5 +1,12 @@
-return function(enemy, accessory)
-	if enemy.skill.hasProjectile then
-		accessory.entity:modifyBattleStat('defense', 1)
+return function(enemy, character)
+	local isTarget = false
+	for _,target in ipairs(enemy.targets) do
+		if target == character then
+			isTarget = true
+		end
+	end
+
+	if isTarget and enemy.skill.isMultihit then
+		character:modifyBattleStat('defense', 1)
 	end
 end;
--- src/logic/item/accessory/designer_loafers.lua
+++ src/logic/item/accessory/designer_loafers.lua
@@ -1,4 +1,3 @@
-return function(characterTeam)
-	local amount = 5
-	characterTeam:gainMoney(amount)
+return function(character)
+	character.landingLag = 0.5 * character.landingLag
 end;
