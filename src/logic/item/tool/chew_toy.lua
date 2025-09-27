b6eb87e22cd96a01997fed7144caf514e3828c9d
cakejamble <campbell.jakelog@gmail.com>
2025-09-27 14:07:06 -0700

    SG-249 Fix chew toy implementation to fit changes to tool signals

--- src/logic/item/tool/chew_toy.lua
+++ src/logic/item/tool/chew_toy.lua
@@ -1,5 +1,7 @@
-return function(enemyTeam)
-	for _,enemy in ipairs(enemyTeam) do
+-- Signal: OnStartCombat
+---@param enemyTeam EnemyTeam
+return function(_, enemyTeam)
+	for _,enemy in ipairs(enemyTeam.members) do
 		enemy:applyStatus('weak')
 	end
 end;
