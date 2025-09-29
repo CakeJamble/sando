85c516dda5c3dbaa2a2378cc39cf256718db5977
cakejamble <campbell.jakelog@gmail.com>
2025-09-28 18:05:12 -0700

    SG-249 Annotations for desecrated idol

--- src/logic/item/tool/desecrated_idol.lua
+++ src/logic/item/tool/desecrated_idol.lua
@@ -1,10 +1,13 @@
 local loadTool = require('util.tool_loader')
 
+-- Signal: OnPickup
+---@param characterTeam CharacterTeam
 return function(characterTeam)
 	local inventory = characterTeam.inventory
 	local newMoney = math.ceil(inventory.money / 2)
 	inventory:setMoney(newMoney)
 
+	-- Add animation?
 	local refurbishedIdol = loadTool('refurbished_idol')
 	inventory:addTool(refurbishedIdol)
 end;
