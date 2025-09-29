d524692545ec8ba5e9cfde55a2c22a6b7f9a86b1
cakejamble <campbell.jakelog@gmail.com>
2025-09-28 18:04:04 -0700

    SG-249 Fixed logic for decroded mitts

--- src/logic/item/tool/decroded_mitts.lua
+++ src/logic/item/tool/decroded_mitts.lua
@@ -1,5 +1,13 @@
-local Character = require('class.entities.character')
+local Signal = require("libs.hump.signal")
 
-return function()
-	Character.canBeDebuffed = true
+-- Signal: OnStartTurn
+---@param character Character
+return function(character)
+	character.debuffImmune = true
+
+	Signal.register("OnEndTurn", function(entity)
+		if entity == character then
+			character.debuffImmune = false
+		end
+	end)
 end;
