0a6faba0a3f5b5b07b7e82667b772b37d571d191
cakejamble <campbell.jakelog@gmail.com>
2025-09-29 12:25:11 -0700

    SG-249 Corrected bugs & incomplete implementations of some tools

--- src/logic/item/tool/land_cucumber.lua
+++ src/logic/item/tool/land_cucumber.lua
@@ -1,4 +1,9 @@
-return function(enemy)
-	local amount = enemy.skill.damage
-	enemy:takeDamagePierce(amount)
+-- Signal: OnFaint
+---@param enemy Enemy
+---@param koCharacters Character[]
+return function(enemy, koCharacters)
+	for _,character in ipairs(koCharacters) do
+		local amount = enemy.skill.damage
+		enemy:takeDamagePierce(amount)
+	end
 end;
--- src/logic/item/tool/loaf_loader.lua
+++ src/logic/item/tool/loaf_loader.lua
@@ -1,8 +1,19 @@
+-- Signal: OnPickup
+---@param characterTeam CharacterTeam
 return function(characterTeam)
-	-- select character
-	local character = characterTeam.members[1]
-	-- select any skill
-	local skill = character.skillPool[1]
-	-- teach them
-	print(skill.cost)
+	local co = coroutine.create(function()
+		local selectedCharacter = characterTeam:yieldCharacterSelect()
+
+		-- pack up 1 random skill from each character
+		local skills = {}
+		for _,character in ipairs(characterTeam.members) do
+			local i = love.math.random(1, #character.skillPool)
+			local skill = character.skillPool[i]
+			table.insert(skills, skill)
+		end
+
+		-- Teach character the skill they select
+		selectedCharacter:yieldSkillSelect(skills)
+	end)
+	coroutine.resume(co)
 end;
