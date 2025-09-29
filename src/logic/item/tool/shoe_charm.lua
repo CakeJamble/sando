79da1676551717a6084ac90bafb7b9c6c8ab8c91
cakejamble <campbell.jakelog@gmail.com>
2025-09-29 14:09:03 -0700

    SG-249 Implemented shoe charm logic

--- src/logic/item/tool/shoe_charm.lua
+++ src/logic/item/tool/shoe_charm.lua
@@ -1,3 +1,8 @@
-return function(character)
-	character:modifyBattleStat('luck', 1)
+-- Signal OnBuff
+---@param character Character
+---@param buff string
+return function(character, buff)
+	if buff == "speed" then
+		character:modifyBattleStat('luck', 1)
+	end
 end;
