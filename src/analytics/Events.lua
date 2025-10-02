8f56da35e3f016cc5ec2aeb989f3bbe1cb608892
cakejamble <campbell.jakelog@gmail.com>
2025-10-01 19:28:54 -0700

    SG-238 Example Posthog event

---
+++ src/analytics/Events.lua
@@ -0,0 +1,15 @@
+local Events = {
+	example = {
+		type = "example",
+		event = "Example Event",
+		properties = {
+			choices = {"Item1", "Item2", "Item3"},
+			selected = "Item",
+			playerName = "cakejamble",
+
+		},
+	},
+}
+
+
+return Events
