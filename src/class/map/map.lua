0e2a10b91f7b82be4c8355e1dec26ed2ad26af56
cakejamble <campbell.jakelog@gmail.com>
2025-10-15 17:30:19 -0700

    SG-274 Set and validate next encounter selection

--- src/class/map/map.lua
+++ src/class/map/map.lua
@@ -1,12 +1,21 @@
 local Class = require('libs.hump.class')
+local flux = require('libs.flux')
 
 ---@class Map
 local Map = Class{}
 
 ---@param mapData table[] 2D Array of Room objects
-function Map:init(mapData)
+---@param numFloors integer
+---@param width integer
+---@param numPaths integer
+function Map:init(mapData, numFloors, width, numPaths)
 	self.mapData = mapData
-	self.lines = {}
+	self.numFloors = numFloors
+	self.width = width
+	self.numPaths = numPaths
+	self.selected = nil
+	self.activeRooms = {}
+	self.selectedIndex = nil
 end;
 
 function Map:connectRooms()
@@ -25,7 +34,95 @@ function Map:connectLines(room)
 	local x1,y1 = room.pos.x + (room.w / 2), room.pos.y + room.h
 	for _,nextRoom in ipairs(room.nextRooms) do
 		local line = {x1, y1, nextRoom.pos.x + (nextRoom.w / 2), nextRoom.pos.y}
-		table.insert(self.lines, line)
+		table.insert(room.branches, line)
+	end
+end;
+
+---@param floor integer
+function Map:checkActiveRooms(floor)
+	for _,room in ipairs(self.mapData[floor]) do
+		if room.type ~= "NA" then
+			if floor == 1 and not room.cleared then
+				room.active = true
+			elseif self:clearedParents(room) then
+				room.active = true
+			end
+
+			if room.active then
+				table.insert(self.activeRooms, room)
+				flux.to(room, 1, {alpha = 1})
+			end
+		end
+	end
+end;
+
+---@param room Room
+---@return boolean
+function Map:clearedParents(room)
+	local parents = {}
+
+	local has = function(list, obj)
+		for _,item in ipairs(list) do
+			if item == obj then
+				return true
+			end
+		end
+		return false
+	end
+
+	-- left parent
+	if room.col > 1 and room.row > 1 then
+		local parentCandidate = self.mapData[room.row - 1][room.col - 1]
+
+		if has(parentCandidate.nextRooms, room) then
+			table.insert(parents, parentCandidate)
+		end
+	end
+
+	-- parent below
+	if room.row > 1 then
+		local parentCandidate = self.mapData[room.row - 1][room.col]
+
+		if has(parentCandidate.nextRooms, room) then
+			table.insert(parents, parentCandidate)
+		end
+	end
+
+	-- right parent
+	if room.col < self.mapWidth and room.row > 1 then
+		local parentCandidate = self.mapData[room.row - 1][room.col + 1]
+
+		if has(parentCandidate.nextRooms, room) then
+			table.insert(parents, parentCandidate)
+		end
+	end
+
+	for _,parent in ipairs(parents) do
+		if parent.cleared then
+			return true
+		end
+	end
+	return false
+end;
+
+---@param joystick love.Joystick
+---@param button love.GamepadButton
+function Map:gamepadpressed(joystick, button)
+	if not self.selected then
+		self.selected = self.activeRooms[1]
+		self.selectedIndex = 1
+	elseif button == "dpleft" then
+		self.selectedIndex = self.selectedIndex - 1
+		if self.selectedIndex <= 0 then
+			self.selectedIndex = #self.activeRooms
+		end
+		self.selected = self.selectedIndex
+	elseif button == "dpright" then
+		self.selectedIndex = self.selectedIndex + 1
+		if self.selectedIndex > #self.activeRooms then
+			self.selectedIndex = 1
+		end
+		self.selected = self.activeRooms[self.selectedIndex]
 	end
 end;
 
@@ -36,9 +133,11 @@ function Map:draw()
 		end
 	end
 
-	for _,line in ipairs(self.lines) do
-		love.graphics.line(line)
+	if self.selected then
+		love.graphics.setColor(0, 0, 1)
+		local x,y,w,h = self.selected.pos.x, self.selected.pos.y, self.selected.w, self.selected.h
+		love.graphics.rectangle("line", x, y, w, h)
+		love.graphics.setColor(1,1,1)
 	end
 end;
-
 return Map
