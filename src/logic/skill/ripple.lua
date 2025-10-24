d849ee9719ad138c2b4bababa9f0fb871f7e9cca
cakejamble <campbell.jakelog@gmail.com>
2025-10-24 11:24:49 -0700

    prototype ripple attack

--- src/logic/skill/ripple.lua
+++ src/logic/skill/ripple.lua
@@ -1,20 +1,43 @@
 local flux = require('libs.flux')
 local Timer = require('libs.hump.timer')
+local Signal = require('libs.hump.signal')
+local Projectile = require('class.entities.projectile')
+local Collision = require('libs.collision')
 
+---@param ref Enemy
 return function(ref)
 	local skill = ref.skill
+	local duration = 1
 	local originX = ref.hitbox.x
-	local targets = ref.target
-	local rippleSpeed = 50
+	local targets = ref.targets
 	local damage = ref.battleStats.attack + skill.damage
+	local luck = ref.battleStats.luck
+	local goalX = -30
+	local tweens = {}
+	local collisions = {}
+	for i,target in ipairs(targets) do
+		local x,y = ref.pos.x - ref.hitbox.w, target.oPos.y + target.hitbox.h
+		local projectile = Projectile(x, y, skill.castsShadow, i)
+		table.insert(ref.projectiles, projectile)
+		table.insert(collisions, false)
 
-	for _,t in ipairs(targets) do
-		local dist = math.abs(t.hitbox.x - originX)
-		local travelTime = dist / rippleSpeed
-		Timer.after(travelTime,
-		function()
-			if not t.isJumping then
-				t:takeDamage(damage)
-		end)
+		local tween = flux.to(projectile.pos, duration, {x = goalX}):ease('quadin')
+			:onupdate(function()
+				if not collisions[i] and Collision.rectsOverlap(projectile.hitbox, target.hitbox) then
+					target:takeDamage(damage, luck)
+					collisions[i] = true
+				end
+			end)
+
+		table.insert(tweens, tween)
 	end
+
+	Timer.after(duration, function()
+		for _,projectile in ipairs(ref.projectiles) do
+			local i = projectile.index
+			table.remove(ref.projectiles, i)
+		end
+		Signal.emit("OnSkillResolved", ref)
+		ref:endTurn(skill.duration, nil, skill.returnTweenType)
+	end)
 end;
