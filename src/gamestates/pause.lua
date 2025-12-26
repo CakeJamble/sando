d3c34dc46a8ca6fe0be49fc372b20ccef29b036c
cakejamble <campbell.jakelog@gmail.com>
2025-12-26 14:47:52 -0800

    SG-327 Added button to restore default settings to pause menu

--- src/gamestates/pause.lua
+++ src/gamestates/pause.lua
@@ -1,4 +1,3 @@
-local SoundManager = require('class.ui.SoundManager')
 local Pause = {}
 local initLuis = require("libs.luis.init")
 local luis = initLuis("libs/luis/widgets")
@@ -6,68 +5,68 @@ local flux = require('libs.flux')
 
 function Pause:init()
     shove.createLayer("ui", {zIndex = 10})
+    self.supportedResolutions = GameSettings.supportedResolutions
     luis.setGridSize(32)
-    self.musicManager = SoundManager(AllSounds.music)
-    self.sfxManager = SoundManager(AllSounds.sfx)
-    self.supportedResolutions = {
-        {640, 360},
-        {1280, 720},
-        {1920, 1080},
-        {2560, 1440},
-        {3840, 2160}
-    }
     self.resIndex = 4
-    -- luis.baseWidth = 2560
-    -- luis.baseHeight = 1440
 end;
 
 function Pause:enter(previous)
-    self.windowWidth, self.windowHeight = shove.getViewportDimensions()
     self.luisTime = 0
-    self.musicManager:play('ny_house_party')
+    GameSettings.musicManager:play('ny_house_party')
 
     self.musicVolSlider = luis.newSlider(0, 1, 1, 10, 1,
         function(value)
-            self.musicManager:setGlobalVolume(value)
+            GameSettings.musicManager:setGlobalVolume(value)
         end, 1, 1)
     self.sfxSlider = luis.newSlider(0, 1, 1, 10, 1,
         function(value)
-        self.sfxManager:setGlobalVolume(value)
+            GameSettings.sfxManager:setGlobalVolume(value)
     end, 3, 1)
     self.sfxButton = luis.newButton("Test", 2, 1, nil,
         function()
-            self.sfxManager:play("uke")
+            GameSettings.sfxManager:play("uke")
         end, 3, 12)
     self.resDownButton = luis.newButton("Smaller", 2, 2, nil,
         function()
             self.resIndex = math.max(1, self.resIndex - 1)
             local newResolution = self.supportedResolutions[self.resIndex]
             local width, height = newResolution[1], newResolution[2]
-            shove.setWindowMode(width, height)
+            GameSettings:setSetting("resolution", {w = width, h = height})
+            -- shove.setWindowMode(width, height)
         end, 6, 1)
     self.resUpButton = luis.newButton("Larger", 2, 2, nil,
         function()
             self.resIndex = math.min(#self.supportedResolutions, self.resIndex + 1)
             local newResolution = self.supportedResolutions[self.resIndex]
             local width, height = newResolution[1], newResolution[2]
-            shove.setWindowMode(width, height)
+            GameSettings:setSetting("resolution", {w = width, h = height})
+            -- shove.setWindowMode(width, height)
         end, 6, 8)
-    self.brightnessSlider = luis.newSlider(0, 1, 1, 10, 1,
+    self.brightnessSlider = luis.newSlider(0, 2, 1, 10, 1,
         function(value)
-            Brightness = value
+            GameSettings.settings.display.brightness = value
+            -- Brightness = value
         end, 8, 1)
-    self.constrastSlider = luis.newSlider(0, 1, 1, 10, 1,
+    self.constrastSlider = luis.newSlider(0, 2, 1, 10, 1,
         function(value)
-            Contrast = value
+            GameSettings.settings.display.contrast = value
+            -- Contrast = value
         end, 10, 1)
-    self.saturationSlider = luis.newSlider(0, 1, 1, 10, 1,
+    self.saturationSlider = luis.newSlider(0, 2, 1, 10, 1,
         function(value)
-            Saturation = value
+            GameSettings.settings.display.saturation = value
+            -- Saturation = value
         end, 12, 1)
     self.hueShiftSlider = luis.newSlider(0, 1, 0, 10, 1,
         function(value)
-            HueShift = value
+            GameSettings.settings.display.hueShift = value
+            -- HueShift = value
         end, 14, 1)
+    self.restoreDefaultsButton = luis.newButton("Restore Defaults", 2, 2, nil,
+        function() 
+            GameSettings:restoreAllDefaults()
+            self:restoreDefaults()
+        end, 16, 1)
     luis.newLayer("main")
     luis.setCurrentLayer("main")
     luis.createElement(luis.currentLayer, "Slider", self.musicVolSlider)
@@ -79,12 +78,14 @@ function Pause:enter(previous)
     luis.createElement(luis.currentLayer, "Slider", self.constrastSlider)
     luis.createElement(luis.currentLayer, "Slider", self.saturationSlider)
     luis.createElement(luis.currentLayer, "Slider", self.hueShiftSlider)
+    luis.createElement(luis.currentLayer, "Button", self.restoreDefaultsButton)
 
     luis.showGrid = true
 end;
 
 function Pause:leave()
     self.musicManager:stopAll()
+    GameSettings:saveSettings()
 end;
 
 ---@return string result
@@ -93,6 +94,15 @@ function Pause:resolutionToString()
     local  width, height = tostring(resolution[1]), tostring(resolution[2])
     local result = width .. ' x ' .. height
     return result
+end;
+
+function Pause:restoreDefaults()
+    self.musicVolSlider.value = 1
+    self.sfxSlider.value = 1
+    self.brightnessSlider.value = 1
+    self.constrastSlider.value = 1
+    self.saturationSlider.value = 1
+    self.hueShiftSlider.value = 0
 end;
 
 function Pause:keypressed(key)
