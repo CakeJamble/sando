d7e4695521c098333451da2ddf2b8323a7bf7003
cakejamble <campbell.jakelog@gmail.com>
2026-01-15 12:26:29 -0800

    SG-322 Added documentation and small input update with baton

--- src/class/entities/LevelUpManager.lua
+++ src/class/entities/LevelUpManager.lua
@@ -4,35 +4,47 @@ local Signal = require('libs.hump.signal')
 local Class = require('libs.hump.class')
 
 ---@class LevelUpManager
-local LevelUpManager = Class{}
+---@field characterTeam CharacterTeam
+---@field levelUpUI { [string]: table }
+---@field duration number
+---@field windowWidth integer
+---@field windowHeight integer
+---@field coroutines table
+---@field i integer
+---@field isDisplayingNotification boolean
+---@field textBox table
+---@field isActive boolean
+local LevelUpManager = Class {}
 
 ---@param characterTeam CharacterTeam
 function LevelUpManager:init(characterTeam)
 	self.characterTeam = characterTeam
 	self.levelUpUI = self.initUI(characterTeam.members)
 	self.duration = 2
-  self.windowWidth, self.windowHeight = shove.getViewportDimensions()
-  -- self.windowWidth, self.windowHeight = push:toReal(self.windowWidth, self.windowHeight)
-  -- self.tx, self.ty = 250, 250
-  self.coroutines = {}
-  self.i = 1
-  self.isDisplayingNotification = false
-  self.textBox = Text.new("left",
-  {
-	    color = {0.9,0.9,0.9,0.95},
-	    shadow_color = {0.5,0.5,1,0.4},
-	    character_sound = true,
-	    sound_every = 2,
-	    -- sound_number = 1
-    })
-  self.isActive = false
+	self.windowWidth, self.windowHeight = shove.getViewportDimensions()
+	-- self.windowWidth, self.windowHeight = push:toReal(self.windowWidth, self.windowHeight)
+	-- self.tx, self.ty = 250, 250
+	self.coroutines = {}
+	self.i = 1
+	self.isDisplayingNotification = false
+	self.textBox = Text.new("left",
+		{
+			color = { 0.9, 0.9, 0.9, 0.95 },
+			shadow_color = { 0.5, 0.5, 1, 0.4 },
+			character_sound = true,
+			sound_every = 2,
+			-- sound_number = 1
+		})
+	self.isActive = false
 end;
 
+--[[Distributes the amount to each member of the team without dividing it.
+Distribution is done one Character at a time using coroutines.]]
 ---@param amount integer
 function LevelUpManager:distributeExperience(amount)
 	self.isActive = true
 	self.coroutines = {}
-	for _,member in ipairs(self.characterTeam.members) do
+	for _, member in ipairs(self.characterTeam.members) do
 		local co = self:createExpCoroutine(member, amount)
 		table.insert(self.coroutines, co)
 	end
@@ -41,9 +53,14 @@ function LevelUpManager:distributeExperience(amount)
 	self:resumeCurrent()
 end;
 
+--[[Adds the exp to the character and procs level ups and new skill learning interactions.
+When a level up occurs, a stat bonus interaction is also triggered here.
+Bypasses the Character method for gaining exp and modifies it directly here.
+A decision needs to be made whether this calls the Character method or does it directly.]]
 ---@param member Character
 ---@param amount integer
 ---@return thread
+---@see Character.gainExp
 function LevelUpManager:createExpCoroutine(member, amount)
 	return coroutine.create(function()
 		local totalExp = amount
@@ -55,11 +72,11 @@ function LevelUpManager:createExpCoroutine(member, amount)
 			local expToGain = math.min(totalExp, member.experienceRequired)
 			totalExp = totalExp - expToGain
 			local expResult = pb:increaseMeter(expToGain)
-			flux.to(pb.meterOptions, self.duration, {width = expResult * pb.mult})
-				:oncomplete(function()
-					member:gainExp(expToGain)
-					self:resumeCurrent()
-				end)
+			flux.to(pb.meterOptions, self.duration, { width = expResult * pb.mult })
+					:oncomplete(function()
+						member:gainExp(expToGain)
+						self:resumeCurrent()
+					end)
 
 			coroutine.yield('await expbar tween')
 			-- trigger level up
@@ -69,7 +86,7 @@ function LevelUpManager:createExpCoroutine(member, amount)
 				Signal.emit('OnLevelUp', member)
 				self:displayNotification(member, function()
 					return member.entityName .. ' leveled up! [shake][color=#0000FF](' .. previousLevel
-						.. ' -> ' .. member.level .. ')[/color][/shake]'
+							.. ' -> ' .. member.level .. ')[/color][/shake]'
 				end)
 				coroutine.yield('levelup')
 				previousLevel = member.level
@@ -80,8 +97,8 @@ function LevelUpManager:createExpCoroutine(member, amount)
 					Signal.emit("OnLearnSkill", member, newSkills)
 					self:displayNotification(member, function()
 						local result = ''
-						for _,skillName in ipairs(newSkills) do
-							result = result .. member.entityName .. ' learned [color=#90EE90]' .. skillName .."[/color]\n"
+						for _, skillName in ipairs(newSkills) do
+							result = result .. member.entityName .. ' learned [color=#90EE90]' .. skillName .. "[/color]\n"
 						end
 						return result
 					end)
@@ -98,22 +115,25 @@ function LevelUpManager:createExpCoroutine(member, amount)
 				-- local statBonus = member:rollStatBonus()
 				-- Signal.emit('OnStatBonus', member, statBonus)
 				self:displayNotification(member, function()
-					local stats = {'hp', 'fp', 'attack', 'defense', 'speed', 'luck'}
+					local stats = { 'hp', 'fp', 'attack', 'defense', 'speed', 'luck' }
 					local i = love.math.random(1, #stats)
 					local j = love.math.random(1, 4)
 					local stat = stats[i]
 					local prevStat = member.baseStats[stat]
 					member.baseStats[stat] = member.baseStats[stat] + j
 					return member.entityName .. "'s " .. stat .. ': [bounce][color=#0000FF]' .. prevStat
-						.. ' -> ' .. member.baseStats[stat] .. '[/color][/bounce]'
+							.. ' -> ' .. member.baseStats[stat] .. '[/color][/bounce]'
 				end)
 				coroutine.yield("statbonus")
 			end
 		end
-			return 'finished'
+		return 'finished'
 	end)
 end;
 
+--[[Controller for `self.coroutines`. Once all coroutines are finished,
+it emits the `OnExpDistributionComplete` signal, which will cause the
+LevelUpManager to relinquish control of the program to the calling object.]]
 function LevelUpManager:resumeCurrent()
 	local co = self.coroutines[self.i]
 	if not co then
@@ -141,6 +161,8 @@ function LevelUpManager:resumeCurrent()
 	end
 end;
 
+--[[Simple abstraction for formatting the text and triggering the SYSL-text handler
+to show the text box and handle player input.]]
 ---@param character Character
 ---@param callback fun(): string
 function LevelUpManager:displayNotification(character, callback)
@@ -150,43 +172,40 @@ function LevelUpManager:displayNotification(character, callback)
 	self.textBox:send(text, 255)
 end;
 
+---@deprecated Uses the deprecated `frameWidth` & `frameHeight` variables
 ---@param members Character[]
 ---@return { [string]: table }
 function LevelUpManager.initUI(members)
 	local uiTable = {}
 
-	for _,member in ipairs(members) do
+	for _, member in ipairs(members) do
 		local pbPos = {
-			x=member.pos.x + (2 * member.frameWidth),
-			y=member.pos.y + (member.frameHeight / 2)
+			x = member.pos.x + (2 * member.frameWidth),
+			y = member.pos.y + (member.frameHeight / 2)
 		}
 		local pbOptions = {
-			xOffset = 0, yOffset = 0,
-			min = 0, max = member.experienceRequired,
-			w = 100, h = 25,
+			xOffset = 0,
+			yOffset = 0,
+			min = 0,
+			max = member.experienceRequired,
+			w = 100,
+			h = 25,
 			wModifier = 0
 		}
-		local blue = {0, 0, 1}
+		local blue = { 0, 0, 1 }
 		local pb = ProgressBar(pbPos, pbOptions, false, blue)
-		uiTable[member.entityName] = {character = member, expBar = pb}
+		uiTable[member.entityName] = { character = member, expBar = pb }
 	end
 
 	return uiTable
 end;
 
----@param joystick love.Joystick
----@param button love.GamepadButton
-function LevelUpManager:gamepadpressed(joystick, button)
-	if button == 'a' and self.isActive then
-		if self.isDisplayingNotification then
-			self.isDisplayingNotification = false
-			self:resumeCurrent()
-		end
-	end
-end;
-
 ---@param dt number
 function LevelUpManager:update(dt)
+	if self.isActive and Player:pressed("confirm") and self.isDisplayingNotification then
+		self.isDisplayingNotification = false
+		self:resumeCurrent()
+	end
 	self.characterTeam:update(dt)
 	if self.isDisplayingNotification then
 		self.textBox:update(dt)
@@ -194,14 +213,14 @@ function LevelUpManager:update(dt)
 end;
 
 function LevelUpManager:draw()
-  love.graphics.push()
+	love.graphics.push()
 
-  love.graphics.setColor(0, 0, 0, 0.6) -- dark transparent background
-  love.graphics.rectangle("fill", 0, 0, self.windowWidth, self.windowHeight)
-  love.graphics.setColor(1, 1, 1)
+	love.graphics.setColor(0, 0, 0, 0.6) -- dark transparent background
+	love.graphics.rectangle("fill", 0, 0, self.windowWidth, self.windowHeight)
+	love.graphics.setColor(1, 1, 1)
 
 	-- love.graphics.translate(self.tx, self.ty)
-	for _,member in pairs(self.levelUpUI) do
+	for _, member in pairs(self.levelUpUI) do
 		member.character:draw()
 		member.expBar:draw()
 		local exp = math.floor(0.5 + member.expBar.meterOptions.width)
@@ -216,7 +235,7 @@ function LevelUpManager:draw()
 		self.textBox:draw(105, 105)
 	end
 
-  love.graphics.pop()
+	love.graphics.pop()
 end;
 
 return LevelUpManager
