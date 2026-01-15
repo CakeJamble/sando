9053bd01675c4326015f090ccfd1e3b982010c90
cakejamble <campbell.jakelog@gmail.com>
2026-01-15 12:27:54 -0800

    SG-322 Bugfixes after documenting some functions

--- src/class/entities/character.lua
+++ src/class/entities/character.lua
@@ -3,7 +3,7 @@ local Entity = require("class.entities.Entity")
 local ActionUI = require("class.ui.ActionUI")
 local Signal = require('libs.hump.signal')
 local Timer = require('libs.hump.timer')
-local flux = require('libs.flux')
+local flux = require('libs.flux.flux')
 local statGrowthFunctions = require('util.calc_new_stats')
 local Class = require "libs.hump.class"
 
@@ -48,7 +48,7 @@ local Class = require "libs.hump.class"
 ---@field jumpHeight integer
 ---@field sfx SoundManager
 ---@field actionUI ActionUI
-local Character = Class{__includes = Entity,
+local Character = Class { __includes = Entity,
   EXP_POW_SCALE = 1.8, EXP_MULT_SCALE = 4, EXP_BASE_ADD = 10,
   -- For testing
   yPos = 500,
@@ -78,6 +78,11 @@ function Character:init(data, actionButton)
   self.growthFunctions = statGrowthFunctions[self.entityName]
   self.currentSkills = {}
   self:updateSkills()
+
+  local animPath = "asset/sprites/entities/character/" .. self.entityName .. "/"
+  self.actor = self:createActor(data.animations, animPath)
+  self.actor:switch('idle')
+
   self.qteSuccess = true
   self.totalExp = 0
   self.experience = 0
@@ -110,12 +115,12 @@ function Character:init(data, actionButton)
 
   Signal.register('OnEnterScene',
     function()
-      flux.to(self.pos, self.combatStartEnterDuration, {x = Character.xPos})
-        :onstart(function() self.actor:switch('run') end)
-        :oncomplete(function()
-          self.actor:switch('idle')
-          self.canJump = true
-        end)
+      flux.to(self.pos, self.combatStartEnterDuration, { x = Character.xPos })
+          :onstart(function() self.actor:switch('run') end)
+          :oncomplete(function()
+            self.actor:switch('idle')
+            self.canJump = true
+          end)
     end
   )
 end;
@@ -132,7 +137,7 @@ function Character:startTurn()
   Signal.emit('OnStartTurn', self)
 end
 
---[[Stores a reference to all valid targets on the field to `self.targetableEntities` 
+--[[Stores a reference to all valid targets on the field to `self.targetableEntities`
 in a table of the format `{characters: Character[], enemies: Enemy[]}`]]
 ---@param targets { [string]: Entity[]}
 ---@param targetType string
@@ -272,7 +277,6 @@ function Character:refresh(amount)
   self.battleStats.fp = math.min(self.baseStats.fp, self.battleStats.fp + amount)
 end;
 
-
 function Character:removeCurses()
   self.curses = {}
 end
@@ -297,7 +301,7 @@ and multiplies them to a copy of the base landing lag.]]
 ---@return number
 function Character:getLandingLag()
   local lag = self.baseLandingLag
-  for _,mult in pairs(self.landingLagMods) do
+  for _, mult in pairs(self.landingLagMods) do
     lag = lag * mult
   end
   return lag
@@ -354,7 +358,7 @@ according to their growth functions. Preserves their HP & FP ratios.]]
 ---@return { string: integer } # Stats from previous level
 function Character:levelUp()
   local oldStats = {}
-  for stat,fcn in ipairs(self.growthFunctions) do
+  for stat, fcn in ipairs(self.growthFunctions) do
     oldStats[stat] = self.baseStats[stat]
     self.baseStats[stat] = fcn(self.level)
 
@@ -371,9 +375,10 @@ end;
 function Character:getRequiredExperience()
   local result
   if self.level < 3 then
-    result = self.level^Character.EXP_POW_SCALE + self.level * Character.EXP_MULT_SCALE + Character.EXP_BASE_ADD
+    result = self.level ^ Character.EXP_POW_SCALE + self.level * Character.EXP_MULT_SCALE +
+        Character.EXP_BASE_ADD
   else
-    result = self.level^Character.EXP_POW_SCALE + self.level * Character.EXP_MULT_SCALE
+    result = self.level ^ Character.EXP_POW_SCALE + self.level * Character.EXP_MULT_SCALE
   end
 
   return result
@@ -384,7 +389,7 @@ containing the names of the skills added.]]
 ---@return string[]
 function Character:updateSkills()
   local result = {}
-  for _,skill in pairs(self.skillPool) do
+  for _, skill in pairs(self.skillPool) do
     if self.level == skill.unlockedAtLvl then
       table.insert(self.currentSkills, skill)
       table.insert(result, skill.name)
@@ -448,22 +453,22 @@ function Character:beginJump()
   self.canJump = false
   -- Goes up then down, then resets conditional checks for guard/jump
   local landY = self.oPos.y
-  local shadow = flux.to(self.shadowDims, Character.jumpDur/2, {w = self.hitbox.w / 3})
-    :ease('quadout')
-    :after(self.shadowDims, Character.jumpDur/2, {w = self.hitbox.w / 2})
+  local shadow = flux.to(self.shadowDims, Character.jumpDur / 2, { w = self.hitbox.w / 3 })
+      :ease('quadout')
+      :after(self.shadowDims, Character.jumpDur / 2, { w = self.hitbox.w / 2 })
       :ease('quadin')
-  local jump = flux.to(self.pos, Character.jumpDur/2, {y = landY - self.jumpHeight})
-    :ease('quadout')
-    :onstart(function() self.actor:switch('jump') end)
-    :after(self.pos, Character.jumpDur/2, {y = landY})
-    :onstart(function() self.actor:switch('fall') end)
-    :ease('quadin')
-    :onupdate(function()
-      if not self.hasLCanceled and landY <= self.pos.y + (self.jumpHeight / 4) then
-        self.canLCancel = true
-      end
-    end)
-    :oncomplete(function() self:land() end)
+  local jump = flux.to(self.pos, Character.jumpDur / 2, { y = landY - self.jumpHeight })
+      :ease('quadout')
+      :onstart(function() self.actor:switch('jump') end)
+      :after(self.pos, Character.jumpDur / 2, { y = landY })
+      :onstart(function() self.actor:switch('fall') end)
+      :ease('quadin')
+      :onupdate(function()
+        if not self.hasLCanceled and landY <= self.pos.y + (self.jumpHeight / 4) then
+          self.canLCancel = true
+        end
+      end)
+      :oncomplete(function() self:land() end)
   self.tweens['jump'] = jump
   self.tweens['shadow'] = shadow
   self.sfx:play("jump")
@@ -482,9 +487,9 @@ function Character:land()
   self.actor:switch('land')
 
   -- Squash on land
-  flux.to(self.pos, 0.125, {sy = 0.4})
-    :ease('quadout')
-    :after(0.125, {sy=0.5})
+  flux.to(self.pos, 0.125, { sy = 0.4 })
+      :ease('quadout')
+      :after(0.125, { sy = 0.5 })
       :ease('quadin')
   Timer.after(landingLag,
     function()
@@ -501,17 +506,17 @@ end
 --[[Usually invoked upon collision, this will stop the progress of a jump and
 place the Character into a tumbling state. Fall-Cancel checks are still applied.]]
 function Character:interruptJump()
-  local tumbleDuration = (Character.jumpDur/2)
+  local tumbleDuration = (Character.jumpDur / 2)
   self:recoil(tumbleDuration)
   local landY = self.oPos.y
   self.tweens['jump']:stop()
-  local tumble = flux.to(self.pos, Character.jumpDur/2, {y=landY}):ease('bouncein')
-    :onupdate(function()
-      if not self.hasLCanceled and landY <= self.pos.y + (self.jumpHeight / 4) then
-        self.canLCancel = true
-      end
-    end)
-    :oncomplete(function() self:land() end)
+  local tumble = flux.to(self.pos, Character.jumpDur / 2, { y = landY }):ease('bouncein')
+      :onupdate(function()
+        if not self.hasLCanceled and landY <= self.pos.y + (self.jumpHeight / 4) then
+          self.canLCancel = true
+        end
+      end)
+      :oncomplete(function() self:land() end)
   self.tweens['tumble'] = tumble
 end;
 
@@ -524,19 +529,41 @@ function Character:updateInput(dt)
   -- Check guard toggle
   if Player:pressed('guardToggle') then
     self.isGuardToggled = not self.isGuardToggled
-  -- Check guard & jump
-  elseif Player:pressed(self.actionButton)  then
+    -- Check guard & jump
+  elseif Player:pressed(self.actionButton) then
     if self.isGuardToggled or Player:down("guard") and self.canGuard then
       self:beginGuard()
-    elseif self.canJump then 
+    elseif self.canJump then
       self:beginJump()
     end
   elseif Player:released("guard") and not self.isGuardToggled then
     self.canGuard = false
-  -- Check L-Cancel
+    -- Check L-Cancel
   elseif Player:pressed("fallCancel") and self.canLCancel then
     self.landingLagMods["LCancel"] = 0.5
     self.hasLCanceled = true
+  end
+end;
+
+--[[----------------------------------------------------------------------------------------------------
+        Animation
+----------------------------------------------------------------------------------------------------]]
+--[[Creates all skill animations from the Character's `self.currentSkills` table.
+This is to avoid slowing down an initial load of skills that may never be used.]]
+---@param dir string The directory where animations are located
+---@param actor table Reference to eh AnimX actor object housing animations
+function Character:createSkillAnimations(dir, actor)
+  for _, skill in ipairs(self.currentSkills) do
+    local skillPath = dir .. skill.tag .. "/"
+    for _, name in ipairs(skill.animations) do
+      local path = skillPath .. name .. ".png"
+      local fullName = skill.tag .. "_" .. name -- ex: needle_stab & wind_up -> needle_stab_wind_up
+      local animation = animx.newAnimation(path)
+      if name == "wobble" or "wobble_fail" then
+        animation:loop()
+      end
+      actor:addAnimation(fullName, animation)
+    end
   end
 end;
 
@@ -560,7 +587,7 @@ end;
 
 function Character:draw()
   Entity.draw(self)
-  love.graphics.setColor(1,1,1)
+  love.graphics.setColor(1, 1, 1)
   if self.actionUI and self.actionUI.active then
     self.actionUI:draw()
   end
